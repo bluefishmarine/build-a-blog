13ddcff6ed18db25311b41598ebe25277dfab875
Gary <garyivanov89@gmail.com>
2018-01-14 19:23:28 -0500

    added more files

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect, render_template, session,flash
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:werto5678@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = "sjfh576b929%&#fj"
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(2000))
+
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blogs.html')
+
+@app.route("/new", methods=['GET','POST'])
+def new_post():
+    return render_template('new.html')
+
+if __name__ == '__main__':
+    app.run()
